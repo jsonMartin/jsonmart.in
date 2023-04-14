eee3ea1d792c1e06dbf3b67ac18834d87f12b046
Jason Martin <4033621+jsonMartin@users.noreply.github.com>
2023-04-13 17:06:11 -0700

    Add overrides from previous site, update for new version for NextJS compatability

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -12,10 +12,11 @@ import 'prismjs/themes/prism-coy.css'
 // core styles shared by all of react-notion-x (required)
 import 'react-notion-x/src/styles.css'
 import 'styles/global.css'
+import 'styles/notion.css'
+import 'styles/override.css'
 // this might be better for dark mode
 // import 'prismjs/themes/prism-okaidia.css'
 // global style overrides for notion
-import 'styles/notion.css'
 // global style overrides for prism theme (optional)
 import 'styles/prism-theme.css'
 
