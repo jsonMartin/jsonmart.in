4fbfe8d8cca9bf62108d07ccdd7309be792f418b
Jason Martin <4033621+jsonMartin@users.noreply.github.com>
2023-04-15 13:07:35 -0700

    Change copyright to uncopyright

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -35,7 +35,9 @@ export const FooterImpl: React.FC = () => {
 
   return (
     <footer className={styles.footer}>
-      <div className={styles.copyright}>Copyright 2023 {config.author}</div>
+      <div className={styles.copyright}>
+        <em>un</em>Copyright 2023 {config.author}
+      </div>
 
       <div className={styles.settings}>
         {hasMounted && (
