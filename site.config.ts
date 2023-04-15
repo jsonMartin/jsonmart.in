ac4692d798b922dafa6a7e3135c67fcf9ab48623
Jason Martin <4033621+jsonMartin@users.noreply.github.com>
2023-04-15 11:28:54 -0700

    Update Youtube channel link

--- site.config.ts
+++ site.config.ts
@@ -21,7 +21,7 @@ export default siteConfig({
   github: 'jsonmartin',
   // mastodon: '#', // optional mastodon profile URL, provides link verification
   // newsletter: '#', // optional newsletter URL
-  youtube: 'everythingisinput', // optional youtube channel name or `channel/UCGbXXXXXXXXXXXXXXXXXXXXXX`
+  youtube: 'channel/UCRVLyPQQwsYGNGlGRRZ0zgQ', // optional youtube channel name or `channel/UCGbXXXXXXXXXXXXXXXXXXXXXX`
 
   // default notion icon and cover images for site-wide consistency (optional)
   // page-specific values will override these site-wide defaults
